fbae104d288b514e7ded6999ae6860e8f2de4f3a
mm-harsha-m <harsha.m@marmeto.com>
2024-03-08 14:23:22 +0530

    final changes

--- assets/global.js
+++ assets/global.js
@@ -14,7 +14,21 @@ document.querySelectorAll('[id^="Details-"] summary').forEach((summary) => {
     summary.setAttribute('aria-controls', summary.nextElementSibling.id);
   }
 
-  summary.addEventListener('click', (event) => {
+  document.querySelectorAll('[id^="Details-"] summary').forEach((summary) => {
+    summary.setAttribute('role', 'button');
+    summary.setAttribute('aria-expanded', summary.parentNode.hasAttribute('open'));
+  
+    if (summary.nextElementSibling.getAttribute('id')) {
+      summary.setAttribute('aria-controls', summary.nextElementSibling.id);
+    }
+  
+    summary.addEventListener('click', (event) => {
+      event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
+    });
+  
+    if (summary.closest('header-drawer, menu-drawer')) return;
+    summary.parentElement.addEventListener('keyup', onKeyUpEscape);
+  });
     event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
   });
 
